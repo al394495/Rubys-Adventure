13dde911858648685a762b708752ec28ae4718c3
QuitijodanN <90702556+QuitijodanN@users.noreply.github.com>
2021-09-28 16:31:35 +0200

    Timing and Framerate

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,17 +7,20 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movimiento Horizontal
         float horizontal = Input.GetAxis("Horizontal");
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
         
-
+        // movimiento vertical
         float vertical = Input.GetAxis("Vertical");
         position.y = position.y + 0.1f * vertical;
 
