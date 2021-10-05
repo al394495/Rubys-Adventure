b747e146fea46f5ab5b6a4fe88da0f6757edf23e
al394495 <al394495@uji.es>
2021-10-05 15:51:47 +0200

    Fix Ruby's Jittering

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
-
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,13 +16,12 @@ public class RubyController : MonoBehaviour
     {
         //Movimiento Horizontal
         float horizontal = Input.GetAxis("Horizontal");
-        Vector2 position = transform.position;
-        position.x = position.x + (3.0f * horizontal) * Time.deltaTime;
-        
-        // movimiento vertical
         float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + (3.0f * horizontal) * Time.deltaTime;
         position.y = position.y + (3.0f * vertical) * Time.deltaTime;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
