ac54d2cb383b9c8b0597183bef081d2dc50ce018
al394495 <al394495@uji.es>
2021-10-15 23:31:01 +0200

    Modifying the RubyController Script

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -17,9 +17,13 @@ public class RubyController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
@@ -28,13 +32,24 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Movimiento Horizontal
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        position = position + move * speed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
@@ -55,6 +70,7 @@ public class RubyController : MonoBehaviour
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            animator.SetTrigger("Hit");
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
