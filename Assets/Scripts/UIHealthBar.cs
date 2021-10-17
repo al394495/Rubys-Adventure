ec5226dfbdb8893bf3cde42b8652ee5adaef1a49
QuitijodanN <al397126@uji.es>
2021-10-17 04:59:51 +0200

    referencias

--- Assets/Scripts/UIHealthBar.cs
+++ Assets/Scripts/UIHealthBar.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class UIHealthBar : MonoBehaviour
 {
+    public static UIHealthBar instance { get; private set; }
+
     public Image mask;
     float originalSize;
+    void Awake()
+    {
+        instance = this;
+    }
 
-    // Start is called before the first frame update
+
     void Start()
     {
         originalSize = mask.rectTransform.rect.width;
